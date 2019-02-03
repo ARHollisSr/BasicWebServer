f00a572582a3c65c152f10d5efe2acdb0f29dde6
FocusedBit <anthonyrayhollis@gmail.com>
2019-02-03 08:26:26 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,53 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-console */
-// eslint-disable-next-line no-undef
-const http = require('http');
 
-const hostname = '127.0.0.1';
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+
+//const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
+const mimeTypes = {
+    'html': 'text/html',
+    'jpeg': 'image/jpeg',
+    'jpg': 'image/jpg',
+    'png': 'image/png',
+    'js': 'text/javascript',
+    'css': 'text/css',
+};
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+const server = http.createServer((req, res) => {
+    let uri = url.parse(req.url).pathname;
+    let fileName = path.join(process.cwd(), unescape(uri));
+
+    console.log('Loading' + uri);
+
+    try {
+        let stats = fs.lstatSync(fileName);
+
+        if(stats.isFile()) {
+            let mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]];
+            res.writeHead(200, {'Content-type': mimeType});
+
+            let fileStream = fs.createReadStream(fileName);
+            fileStream.pipe(res);
+        }
+        else if(stats.isDirectory()) {
+            res.writeHead(302, {'Location': 'index.html'});
+            res.end();
+        }
+        else {
+            res.writeHead(500, {'Content-type': 'text/plain'});
+            res.write('500 Internal Error\n');
+            res.end();
+        }
+    } 
+    catch (e) {
+        res.writeHead(404, {'Content-type': 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+}).listen(3000);

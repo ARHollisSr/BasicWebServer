be35f2921ac9ecaf5392d672a3c947a5b1e91c1b
FocusedBit <anthonyrayhollis@gmail.com>
2019-02-02 21:42:51 -0500

    added server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line no-undef
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
